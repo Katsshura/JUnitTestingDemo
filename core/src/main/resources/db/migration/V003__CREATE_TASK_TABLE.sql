7e45c2f76a7398d8cfc878c4fc0ee7cbb935da32
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-27 02:08:09 -0300

    fixed migrations

--- core/src/main/resources/db/migration/V003__CREATE_TASK_TABLE.sql
+++ core/src/main/resources/db/migration/V003__CREATE_TASK_TABLE.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS "task"
 (
     id          BIGSERIAL PRIMARY KEY,
     created_at  TIMESTAMP NOT NULL DEFAULT now(),
-    updated_at  TIMESTAMP NOT NULL DEFAULT now(),
     description varchar(255),
     user_id     BIGINT,
     CONSTRAINT fk_task_user_id
