aefc9b06bd827fc8d5ff6170f8cb644d069da61a
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-28 04:41:25 -0300

    Created sql scripts to be executed before/after method test execution

---
+++ core/src/test/resources/scripts/BeforeUserRepositoryTest.sql
@@ -0,0 +1,6 @@
+insert into "user" (id, created_at, email, first_name, last_name, full_name) values (1, now(), 'test1@gmail.com', 'test', 'one', 'test one');
+insert into "user" (id, created_at, email, first_name, last_name, full_name) values (2, now(), 'vincenzo@test.com', 'vincenzo', 'marshall', 'vincenzo marshall');
+insert into "user" (id, created_at, email, first_name, last_name, full_name) values (3, now(), 'shanai@test.com', 'shanai', 'armstrong', 'shanai hilton armstrong');
+insert into "user" (id, created_at, email, first_name, last_name, full_name) values (4, now(), 'cheyanne@test.com', 'cheyanne', 'kelley', 'cheyanne connelly kelley');
+insert into "user" (id, created_at, email, first_name, last_name, full_name) values (5, now(), 'ajwa@test.com', 'ajwa', 'thompson', 'ajwa thompson');
+insert into "user" (id, created_at, email, first_name, last_name, full_name) values (6, now(), 'test2@something.any', 'lorem', 'ipsum', 'lorem ipsum');
