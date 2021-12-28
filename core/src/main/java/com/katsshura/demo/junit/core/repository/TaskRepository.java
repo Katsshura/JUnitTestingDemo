062ba258a36ab1bf8bf2db3de5a2fc4e208e635a
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-28 05:43:34 -0300

    added query to find task by user id

--- core/src/main/java/com/katsshura/demo/junit/core/repository/TaskRepository.java
+++ core/src/main/java/com/katsshura/demo/junit/core/repository/TaskRepository.java
@@ -2,6 +2,12 @@ package com.katsshura.demo.junit.core.repository;
 
 import com.katsshura.demo.junit.core.entities.task.TaskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+    @Query(value = "SELECT t FROM TaskEntity t WHERE t.user.id = :userId")
+    List<TaskEntity> findByUserId(@Param("userId") Long userId);
 }
