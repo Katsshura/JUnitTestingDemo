5310f1ad3e2d6511f1324277f78506539c36f58c
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-28 04:39:12 -0300

    updated migration constraint to make email column unique

--- core/src/main/resources/db/migration/V002__CREATE_USER_TABLE.sql
+++ core/src/main/resources/db/migration/V002__CREATE_USER_TABLE.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "user"
 (
     id         BIGSERIAL PRIMARY KEY,
     created_at TIMESTAMP NOT NULL DEFAULT now(),
-    email      varchar(255) NOT NULL,
+    email      varchar(255) NOT NULL UNIQUE,
     first_name varchar(255) NOT NULL,
     last_name varchar(255) NOT NULL,
     full_name varchar(255) NOT NULL
