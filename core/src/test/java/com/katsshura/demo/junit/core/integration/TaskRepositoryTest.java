70910a824e3f9dfa0055534391b5c548dfaef617
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-28 05:44:18 -0300

    Added listing task testing

--- core/src/test/java/com/katsshura/demo/junit/core/integration/TaskRepositoryTest.java
+++ core/src/test/java/com/katsshura/demo/junit/core/integration/TaskRepositoryTest.java
@@ -4,6 +4,8 @@ import com.katsshura.demo.junit.core.config.IntegrationTestsConfiguration;
 import com.katsshura.demo.junit.core.entities.task.TaskEntity;
 import com.katsshura.demo.junit.core.entities.user.UserEntity;
 import com.katsshura.demo.junit.core.repository.TaskRepository;
+import net.logstash.logback.encoder.org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -12,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
+
+import java.util.Objects;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -80,6 +84,71 @@ public class TaskRepositoryTest {
     @Nested
     @IntegrationTestsConfiguration
     class ListTask {
+
+        @Test
+        @DisplayName("Should assertNotNull and Equals list size 18 when retrieving all entries from DB")
+        @SqlGroup({
+                @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                        scripts = { "classpath:scripts/user/BeforeUserRepositoryTest.sql",
+                                "classpath:scripts/task/BeforeTaskRepositoryTest.sql"}),
+                @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                        scripts = { "classpath:scripts/task/AfterTaskRepositoryTest.sql",
+                                "classpath:scripts/user/AfterUserRepositoryTest.sql"}),
+        })
+        public void findAllTasksInDatabase() {
+            final int expectedListSize = 18;
+
+            final var result = taskRepository.findAll();
+
+            assertAll(
+                    () -> assertNotNull(result),
+                    () -> assertEquals(expectedListSize, result.size())
+            );
+        }
+
+        @ParameterizedTest(name = "#[{index}] Should assertNotNull and assertEquals for all entity properties when" +
+                " find by id, parameters values: userId = {0} | description = {1} | id = {2}")
+        @CsvFileSource(resources = "/csv/task/TaskTestValidEntries.csv", numLinesToSkip = 1)
+        @SqlGroup({
+                @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                        scripts = { "classpath:scripts/user/BeforeUserRepositoryTest.sql",
+                                "classpath:scripts/task/BeforeTaskRepositoryTest.sql"}),
+                @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                        scripts = { "classpath:scripts/task/AfterTaskRepositoryTest.sql",
+                                "classpath:scripts/user/AfterUserRepositoryTest.sql"}),
+        })
+        public void findTaskById(final Long userId, final String description, final Long id) {
+            final var result = taskRepository.findById(id);
+
+            assertAll(
+                    () -> assertTrue(result.isPresent()),
+                    () -> assertEquals(description, result.get().getDescription()),
+                    () -> assertEquals(userId, result.get().getUser().getId())
+            );
+
+        }
+
+        @ParameterizedTest(name = "#[{index}] Should assertNotNull and assertEquals for all entity properties when" +
+                " find by user id, parameters values: userId = {0} | description = {1}")
+        @CsvFileSource(resources = "/csv/task/TaskTestValidEntries.csv", numLinesToSkip = 1)
+        @SqlGroup({
+                @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD,
+                        scripts = { "classpath:scripts/user/BeforeUserRepositoryTest.sql",
+                                "classpath:scripts/task/BeforeTaskRepositoryTest.sql"}),
+                @Sql(executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD,
+                        scripts = { "classpath:scripts/task/AfterTaskRepositoryTest.sql",
+                                "classpath:scripts/user/AfterUserRepositoryTest.sql"}),
+        })
+        public void findTaskByUserId(final Long userId, final String description) {
+            final var result = taskRepository.findByUserId(userId);
+
+            assertAll(
+                    () -> assertFalse(result.isEmpty()),
+                    () -> assertTrue(result.stream().noneMatch(t -> StringUtils.isEmpty(t.getDescription()))),
+                    () -> assertTrue(result.stream().anyMatch(t -> Objects.equals(t.getDescription(), description))),
+                    () -> assertTrue(result.stream().allMatch(t -> Objects.equals(t.getUser().getId(), userId)))
+            );
+        }
 
     }
 
