e537dea3d0a69607f0b694ef81a763638df2bb76
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-28 04:38:52 -0300

    Added finding by email repository method

--- core/src/main/java/com/katsshura/demo/junit/core/repository/UserRepository.java
+++ core/src/main/java/com/katsshura/demo/junit/core/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.katsshura.demo.junit.core.repository;
 import com.katsshura.demo.junit.core.entities.user.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
