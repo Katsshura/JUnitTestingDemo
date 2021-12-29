46f14ac4dc8e9107eaf35aa83d5730e4214c0288
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-29 01:34:48 -0300

    Created config for exception handler

---
+++ api/src/main/java/com/katsshura/demo/junit/api/handler/ExceptionHandlerConfig.java
@@ -0,0 +1,34 @@
+package com.katsshura.demo.junit.api.handler;
+
+import com.katsshura.demo.junit.api.response.ResponseBuilder;
+import com.katsshura.demo.junit.api.response.ResponseError;
+import com.katsshura.demo.junit.core.exceptions.InvalidNameException;
+import com.katsshura.demo.junit.core.exceptions.UserNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+@Controller
+public class ExceptionHandlerConfig {
+
+    private final ResponseBuilder responseBuilder;
+
+    public ExceptionHandlerConfig(ResponseBuilder responseBuilder) {
+        this.responseBuilder = responseBuilder;
+    }
+
+    @ExceptionHandler(InvalidNameException.class)
+    public final ResponseEntity<ResponseError> handleInvalidNameException(final InvalidNameException e) {
+        return responseBuilder.buildErrorResponse(e, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public final ResponseEntity<ResponseError> handleUserNotFoundException(final UserNotFoundException e) {
+        return responseBuilder.buildErrorResponse(e, HttpStatus.BAD_REQUEST);
+    }
+}
