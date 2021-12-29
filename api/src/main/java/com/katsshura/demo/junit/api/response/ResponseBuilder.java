315559aa68f70c32e56c90dec0e9d7e221407a51
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-29 01:34:30 -0300

    created response builder

---
+++ api/src/main/java/com/katsshura/demo/junit/api/response/ResponseBuilder.java
@@ -0,0 +1,45 @@
+package com.katsshura.demo.junit.api.response;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Collection;
+
+@Service
+public class ResponseBuilder {
+
+    public ResponseEntity<ResponseError> buildErrorResponse(final Exception cause, final HttpStatus statusCode) {
+        final ResponseError response = ResponseError.builder().reason(cause).build();
+        return new ResponseEntity<>(response, statusCode);
+    }
+
+    public <T> ResponseEntity<T> buildCreatedResponse(final Object responseObjectId) {
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseObjectId)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    public <T> ResponseEntity<T> buildCollectionResponseOkOrNoContent(final T value) {
+        if (value == null || ((Collection<?>) value).isEmpty()) {
+            return buildResponseOkOrNoContent(null);
+        }
+
+        return buildResponseOkOrNoContent(value);
+    }
+
+    public  <T> ResponseEntity<T> buildResponseOkOrNoContent(final T value) {
+        if (value == null) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok(value);
+    }
+}
---
+++ api/src/main/java/com/katsshura/demo/junit/api/response/ResponseError.java
@@ -0,0 +1,14 @@
+package com.katsshura.demo.junit.api.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class ResponseError {
+    @JsonIgnoreProperties(value = {"stackTrace", "cause", "suppressed", "localizedMessage"}, allowSetters = true)
+    @JsonProperty("errorReason")
+    private final Exception reason;
+}
