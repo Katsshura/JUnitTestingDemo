c892f441a9b115a808a8fdb59ebba23cb54addc2
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2021-12-30 02:25:10 -0300

    Created ApplicationContext loader

---
+++ api/src/test/java/com/katsshura/demo/junit/api/util/ApplicationContextProvider.java
@@ -0,0 +1,21 @@
+package com.katsshura.demo.junit.api.util;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApplicationContextProvider implements ApplicationContextAware{
+
+    private static ApplicationContext context;
+
+    public static ApplicationContext getApplicationContext() {
+        return context;
+    }
+
+    @Override
+    public void setApplicationContext(final ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
+}
